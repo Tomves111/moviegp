97e751511cbae6612827bc7ffc55181ae58bfb3b
tomves111 <tomves111@gmail.com>
2020-07-04 19:34:08 +0200

    Updates

---
+++ precache-manifest.bb2d568aa875e2a578ecbc8a4c99806b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea737a722362764241e871c5fc73d17",
+    "url": "/moviesearchgp/index.html"
+  },
+  {
+    "revision": "afea2e70fbdfb0eb263d",
+    "url": "/moviesearchgp/static/css/main.c0d00140.chunk.css"
+  },
+  {
+    "revision": "75b4f90e051e643d4650",
+    "url": "/moviesearchgp/static/js/2.906eed52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviesearchgp/static/js/2.906eed52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afea2e70fbdfb0eb263d",
+    "url": "/moviesearchgp/static/js/main.f0390720.chunk.js"
+  },
+  {
+    "revision": "36044be1d3f487e6e830",
+    "url": "/moviesearchgp/static/js/runtime-main.f52b346e.js"
+  }
+]);
